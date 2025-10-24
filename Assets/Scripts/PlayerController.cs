d3699841af9349060cdb37d6ada29411fb18d7a2
Yaamiii1 <valentina_savini@studenti.naba.it>
2025-10-24 10:56:00 +0200

    Limit Range

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 15.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,9 +12,18 @@ public class NewMonoBehaviourScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() 
+    { 
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-15, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > +xRange)
+        {
+            transform.position = new Vector3(+15, transform.position.y, transform.position.z);
+        }
     }
 }
