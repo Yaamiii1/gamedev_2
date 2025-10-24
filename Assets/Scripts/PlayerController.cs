ee26b3b6f568a3c6b8b46e10eb7b4138a6750934
Yaamiii1 <valentina_savini@studenti.naba.it>
2025-10-24 12:16:21 +0200

    Food projectile

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,20 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    public GameObject projectilePrefab;
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 15.0f;
+  
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update() 
-    { 
+    void Update()
+    {
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
@@ -24,6 +26,14 @@ public class NewMonoBehaviourScript : MonoBehaviour
         if (transform.position.x > +xRange)
         {
             transform.position = new Vector3(+15, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+
+        {
+            Instantiate(projectilePrefab,transform.position + Vector3.right * 0.1f, transform.rotation);
+
+
         }
     }
 }
